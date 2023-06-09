798de83453674ab15dc676dd2bd4bf62b3bfbe11
charlieze1 <134844104+charlieze1@users.noreply.github.com>
2023-06-09 05:24:18 -0700

    printing

---
+++ main.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("say something am listening")
+    audio = r.listen(source)
+
+try:
+    print("you said: " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("don't understand speak again")
+
